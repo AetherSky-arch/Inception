da1c32698e1bd13c481ee536e78454942125db27
Ari Guez <arguez@bess-f2r5s2.clusters.42paris.fr>
2025-01-17 19:41:40 +0100

    fix: fix site redirecting to url 'localhost'

--- requirements/wordpress/get_site.sh
+++ requirements/wordpress/get_site.sh
@@ -5,6 +5,6 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=$ADMINUSR --admin_password=$ADMINPASSWD --admin_email=admin@admin.com --allow-root
+./wp-cli.phar core install --url=arguez.42.fr --title=inception --admin_user=$ADMINUSR --admin_password=$ADMINPASSWD --admin_email=admin@admin.com --allow-root
 
 php-fpm7.4 -F
