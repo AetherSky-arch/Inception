2a49084996b4a96fb5f00a8d2dcda7bc077c3c04
Ari Guez <arguez@bess-f2r2s13.clusters.42paris.fr>
2025-01-20 16:57:32 +0100

    fix: fix scp call

--- VMsetup.sh
+++ VMsetup.sh
@@ -29,7 +29,7 @@ echo "INFO: Booting up VM"
 vagrant up
 
 echo "INFO: Installing packages, you may be asked to enter your password a few times. default password is \'vagrant\'"
-scp -rp $ORIGIN_DIR/requirements $ORIGIN_DIR/docker-compose.yml $ORIGIN_DIR/install_packages.sh vagrant@127.0.0.1:~ -P 2222
+scp -rp -P 2222 $ORIGIN_DIR/requirements $ORIGIN_DIR/docker-compose.yml $ORIGIN_DIR/install_packages.sh vagrant@127.0.0.1:~
 ssh vagrant@127.0.0.1 -p 2222 sudo bash install_packages.sh
 
 echo "INFO: setup completed!"
