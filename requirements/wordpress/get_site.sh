ed21705d543f14b0ea0c3d108d75fc11e5935908
Ari Guez <arguez@bess-f2r2s13.clusters.42paris.fr>
2025-01-20 16:56:44 +0100

    fix: revert db credentials to default values

--- requirements/wordpress/get_site.sh
+++ requirements/wordpress/get_site.sh
@@ -4,7 +4,7 @@ cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=$DBUSR --dbpass=$DBPASSWD --dbhost=mariadb --allow-root
+./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
 ./wp-cli.phar core install --url=arguez.42.fr --title=inception --admin_user=$ADMINUSR --admin_password=$ADMINPASSWD --admin_email=admin@admin.com --allow-root
 
 php-fpm7.4 -F
